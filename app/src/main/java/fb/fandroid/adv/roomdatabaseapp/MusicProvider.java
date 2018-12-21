dee9565d2f85e03be9b2044a615a58e64c5eb509
savelgmx <savel@gmx.com>
2018-12-21 16:08:24 +0700

    #6 commit
      //TODO По аналогии добавить в MusicProvider работу с таблицами Songs и AlbumSongs.
        //TODO Дописать методы Cursor query insert update delete

--- app/src/main/java/fb/fandroid/adv/roomdatabaseapp/MusicProvider.java
+++ app/src/main/java/fb/fandroid/adv/roomdatabaseapp/MusicProvider.java
@@ -14,16 +14,23 @@ import fb.fandroid.adv.roomdatabaseapp.database.MusicDao;
 import fb.fandroid.adv.roomdatabaseapp.database.MusicDatabase;
 
 public class MusicProvider extends ContentProvider {
-//TODO По аналогии добавить в MusicProvider работу с таблицами Songs и AlbumSongs.
+    //TODO По аналогии добавить в MusicProvider работу с таблицами Songs и AlbumSongs.
+    //TODO Дописать методы Cursor query insert update delete
     private static final String TAG = MusicProvider.class.getSimpleName();
 
     private static final String AUTHORITY = "com.elegion.roomdatabase.musicprovider";
     private static final String TABLE_ALBUM = "album";
+    public static final String TABLE_SONG ="song";
+    public static final String TABLE_ALBUMSONG="albumsong";
 
     private static final UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
 
     private static final int ALBUM_TABLE_CODE = 100;
     private static final int ALBUM_ROW_CODE = 101;
+    private static final int SONG_TABLE_CODE=200;
+    private static final int SONG_ROW_CODE=201;
+    private static final int ALBUMSONG_TABLE_CODE=300;
+    private static final int ALBUMSONG_ROW_CODE=301;
 
     static {
         URI_MATCHER.addURI(AUTHORITY, TABLE_ALBUM, ALBUM_TABLE_CODE);
@@ -54,6 +61,14 @@ public class MusicProvider extends ContentProvider {
                 return "vnd.android.cursor.dir/" + AUTHORITY + "." + TABLE_ALBUM;
             case ALBUM_ROW_CODE:
                 return "vnd.android.cursor.item/" + AUTHORITY + "." + TABLE_ALBUM;
+            case SONG_TABLE_CODE:
+                return "vnd.android.cursor.dir/" + AUTHORITY+"." + TABLE_SONG;
+            case SONG_ROW_CODE:
+                return "vnd.android.cursor.item/" + AUTHORITY+"." + TABLE_SONG;
+            case ALBUMSONG_TABLE_CODE:
+                return "vnd.android.cursor.dir/" + AUTHORITY+"." + TABLE_ALBUMSONG;
+            case ALBUMSONG_ROW_CODE:
+                return "vnd.android.cursor.item/" + AUTHORITY+"." + TABLE_ALBUMSONG;
             default:
                 throw new UnsupportedOperationException("not yet implemented");
         }
@@ -65,9 +80,25 @@ public class MusicProvider extends ContentProvider {
 
         int code = URI_MATCHER.match(uri);
 
-        if (code != ALBUM_ROW_CODE && code != ALBUM_TABLE_CODE) return null;
+        if ((code != ALBUM_ROW_CODE) && (code != ALBUM_TABLE_CODE)
+                && (code != SONG_ROW_CODE) && (code != SONG_TABLE_CODE)
+                && (code != ALBUMSONG_ROW_CODE) && (code != ALBUMSONG_TABLE_CODE)
+                ) return null;
 
         Cursor cursor;
+
+        switch (code)
+        {
+            case ALBUM_TABLE_CODE:
+                cursor = mMusicDao.getAlbumsCursor();
+                return cursor;
+            case ALBUM_ROW_CODE:
+                cursor = mMusicDao.getAlbumWithIdCursor((int) ContentUris.parseId(uri));
+                return cursor;
+
+
+        }
+
 
         if (code == ALBUM_TABLE_CODE) {
             cursor = mMusicDao.getAlbumsCursor();
